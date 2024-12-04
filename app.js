3c621586f75168a8fcb5ce7102d0f17ac9f8ca47
aznshrllh <ahmadzakinashirullah@gmail.com>
2024-12-04 10:36:09 +0700

    feat: socket.io server

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running at: http://localhost:${port}`);
+});
