f58b448fe7b74b6f282dd979dca93dc82a05ae75
aznshrllh <ahmadzakinashirullah@gmail.com>
2024-12-05 10:08:12 +0700

    fixing:https nya

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.get("/", (req, res) => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://tacticular.web.app",
   },
 });
 
